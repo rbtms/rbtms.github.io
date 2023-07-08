7a17da7dd6fb33820877f7d20bfe3a01b9f69cc0
Alvaro <alvaro@wearenumber1.com>
2023-07-08 23:30:13 +0200

    Widen click area of pdf buttons

--- js/index.js
+++ js/index.js
@@ -23,3 +23,6 @@ $(".sidenav-link").on("click", function() {
   }
 });
 
+/* Widen the click area of the a tags inside the buttons */
+$($('button')[0]).on('click', () => $('.a-pdf')[0].click() )
+$($('button')[1]).on('click', () => $('.a-pdf')[1].click() )
